397a0ffd551b9330c45ecb0ae481a6b9207f9479
Suleman Pir <suleman_mohiuddin@yahoo.com>
2024-06-20 20:58:06 +0500

    first day

---
+++ Variables.py
@@ -0,0 +1,7 @@
+price=10
+name = "suleman-"
+sname = 'test '
+print((name + sname) * price)
+
+rating = 2.0
+print(rating)
---
+++ input.py
@@ -0,0 +1,15 @@
+# name = input("what is ur name ")
+# print("Hello " + name)
+
+num1 = float(input("enter num 1 = "))
+num2 = float(input("enter num 2 = "))
+
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+
+print(f"Add = {add}")
+print(f"Sub = {sub}")
+print(f"mul = {mul}")
+print(f"div = {div}")
